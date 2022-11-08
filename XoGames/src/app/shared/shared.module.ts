1b19c4a8efbaf2de64aa637e9d782ee782735099
Vigen <vigenhovhannisian@gmail.com>
2022-11-08 09:39:15 +0400

    add comma pipe

--- XoGames/src/app/shared/shared.module.ts
+++ XoGames/src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FooterComponent} from "./footer/footer.component";
 import {HeaderComponent} from "./header/header.component";
 import {IconComponent} from "./icon/icon.component";
@@ -31,7 +31,7 @@ import {MaterialModule} from "./material.module";
     IconComponent,
     LivePointComponent,
     SlickCarouselModule,
-    MaterialModule
+    MaterialModule,
   ]
 })
 export class SharedModule { }
